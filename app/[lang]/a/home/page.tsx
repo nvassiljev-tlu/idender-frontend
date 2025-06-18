79ea8fa60d0483ebc67d491cc16dae8837451485
Jonas Oliveira <jonaso@tlu.ee>
2025-06-18 12:52:04 +0300

    Fixed home page padding

--- app/[lang]/a/home/page.tsx
+++ app/[lang]/a/home/page.tsx
@@ -143,8 +143,8 @@ export default function IdenderDashboard() {
   }
 
   return (
-    <div className="min-h-screen bg-slate-200 text-slate-800 flex flex-col min-w-[320px] rounded-t-2xl">
-      <main className="w-full max-w-6xl mx-auto p-4 flex-1 overflow-y-auto pt-16 min-w-[320px]">
+    <div className="min-h-screen bg-slate-200 text-slate-800 flex flex-col min-w-[320px]">
+      <main className="w-full max-w-6xl mx-auto p-4 flex-1 overflow-y-auto min-w-[320px]">
         <section className="mb-20 w-full max-w-5xl min-w-[320px] sm:min-w-[600px] mx-auto">
           <div className="flex items-center gap-2 mb-4">
             <NewspaperIcon />
--- app/[lang]/a/layout.tsx
+++ app/[lang]/a/layout.tsx
@@ -8,7 +8,7 @@ export default function FolderLayout({
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <div className="flex-1 overflow-y-auto pt-16">
+      <div className="flex-1 overflow-y-auto">
         {children}
       </div>
     </div>
